e12906fdab089168c537e1599b0b3f2a48208dbc
alen <grafuxa@gmail.com>
2020-11-17 11:57:01 +0100

    first push to repo

---
+++ src/components/Hero.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="hero  is-info is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Primary bold titlea</h1>
+        <h2 class="subtitle">Primary bold subtitle</h2>
+      </div>
+    </div>
+  </section>
+</template>
