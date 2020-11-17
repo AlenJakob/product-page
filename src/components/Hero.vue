d87f9dbcd8b3d867edb2cc6330c2edda55df1361
alen <grafuxa@gmail.com>
2020-11-17 15:11:00 +0100

    navbar & hero Added

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,15 @@
 <template>
-  <section class="hero  is-info is-bold">
+  <section class="hero is-medium is-info is-bold">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">Primary bold titlea</h1>
-        <h2 class="subtitle">Primary bold subtitle</h2>
+        <h1 class="title">Shouter</h1>
+        <h2 class="subtitle">Best shoes ever</h2>
       </div>
     </div>
   </section>
 </template>
+
+
+<style>
+
+</style>
