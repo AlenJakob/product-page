1f7055f68e385fbd6f45d7edf084978a07cbaf8d
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-11-22 21:37:33 +0100

    position text in carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,6 +3,7 @@
     <b-carousel-item v-for="(item, i) in carousels" :key="i" :has-drag="drag">
       <span class="image">
         <img :src="item.image" />
+        <h1 class="title title-inside">{{ item.title }}</h1>
       </span>
     </b-carousel-item>
   </b-carousel>
@@ -21,25 +22,25 @@ export default {
       indicatorStyle: "is-dots",
       carousels: [
         {
-          title: "Slide 1",
+          title: "Best Tricks",
           color: "info",
           image:
             "https://cdn.pixabay.com/photo/2019/06/25/11/11/mountain-bike-4297972_1280.jpg",
         },
         {
-          title: "Slide 2",
+          title: "Mountain",
           color: "success",
           image:
             "https://cdn.pixabay.com/photo/2020/11/08/15/36/mountain-5724055_1280.jpg",
         },
         {
-          title: "Slide 3",
+          title: "Dark forest",
           color: "warning",
           image:
             "https://cdn.pixabay.com/photo/2015/07/02/10/13/cycling-828646_1280.jpg",
         },
         {
-          title: "Slide 4",
+          title: "Fast Travel",
           color: "danger",
           image:
             "https://cdn.pixabay.com/photo/2016/11/18/21/26/bike-1836934_1280.jpg",
@@ -52,8 +53,8 @@ export default {
 </script>
 
 <style >
-.carousel-indicator{
-    background: rgba(65, 105, 225, 0.473);
+.carousel-indicator {
+  background: rgba(65, 105, 225, 0.473);
 }
 .carousel-item .image img {
   object-fit: cover;
@@ -68,8 +69,17 @@ export default {
   background: #167df0;
   border: 1px solid white;
 }
-.carousel .carousel-indicator .indicator-item .indicator-style.is-dots{
-    width: 35px;
-    height: 15px;
+.carousel .carousel-indicator .indicator-item .indicator-style.is-dots {
+  width: 35px;
+  height: 15px;
+}
+.title-inside {
+  position: absolute;
+  color: #fff;
+  text-shadow: 0 0 12px black;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
 }
 </style>
