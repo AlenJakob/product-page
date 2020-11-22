dca35b1b9da708ee3dcea2f6dc5b37e8dcf94a52
Alen <33994452+AlenJakob@users.noreply.github.com>
2020-11-22 17:25:57 +0100

    added 1 more record for test

--- src/components/sections/Section-1.vue
+++ src/components/sections/Section-1.vue
@@ -1,28 +1,28 @@
 <template>
   <section class="section has-text-right ml-6 mr-5">
-    <div class="container" v-for="bike in Bikes" :key="bike.id">
+    <div class="container mb-6" v-for="bike in Bikes" :key="bike.id">
       <div class="card">
         <div class="card-image">
           <figure class="image is-2by1">
             <img :src="bike.img" alt="Placeholder image" />4
-            <h1>{{bike.img}}</h1>
+            <h1 class="title">{{ bike.img }}</h1>
           </figure>
         </div>
         <div class="card-content">
           <div class="media">
             <div class="media-content">
               <p class="title is-4">{{ bike.title }}</p>
-              <p class="subtitle is-6">@johnsmith</p>
+              <p class="subtitle is-6 has-text-danger">#{{ bike.name }}</p>
             </div>
           </div>
 
           <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            nec iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-            <a href="#">#responsive</a>
+            {{ bike.content }}
+            <br />
             <br />
             <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
           </div>
+          <button class="button is-info">read more</button>
         </div>
       </div>
     </div>
